83137d022e64240ef67890be7dfd6b0503788457
Konstantin <jashilko@gmail.com>
2021-10-19 22:02:59 +0300

    Выравниваем версии

--- 1.py
+++ 1.py
@@ -6,8 +6,8 @@ from telebot import types
 
 token = os.environ['tg_token']
 
-START, PHOTO, RATE, CATEGORY, DESCR, FINISH = range(6)
-USER_STATE = defaultdict(lambda: START)
+ZERO, START, PHOTO, RATE, CATEGORY, DESCR, FINISH = range(7)
+USER_STATE = defaultdict(lambda: ZERO)
 
 db_worker = PSQLighter()
 
@@ -45,6 +45,9 @@ def callback_handler(callback_query):
     if text == 'canceldescr':
         update_state(message, FINISH)
 
+@bot.message_handler(commands=['start'])
+def handle_message(message):
+    update_state(message, START)
 
 # Начальное состояние → Просим фото.
 @bot.message_handler(func=lambda message: get_state(message) == START)
@@ -60,7 +63,9 @@ def handle_message(message):
     # Проверка пользователя на существование
     db_worker.check_exist_client(message.chat)
     # Пользователь прислал описание или фото
-    if message.photo is None:
+    if message.content_type == 'photo':
+        db_worker.set_photo(message)
+    else:
         db_worker.set_descr(message)
 
     keyboard = types.InlineKeyboardMarkup(row_width=2)
@@ -90,6 +95,19 @@ def handle_message(message):
     bot.send_message(chat_id=message.chat.id, text='Категория #' + message.text +
                                                    ' установлена. Хочешь что-то добавить к описанию?', reply_markup=keyboard)
     update_state(message, FINISH)
+
+
+# Прислали цифру. Выводим top последних
+@bot.message_handler(func=lambda message: message.text.isdigit())
+def handle_digit(message):
+    bot.send_message(chat_id=message.chat.id, text='Ты прислал только цифры')
+    db_worker.get_lasts(message)
+
+# Прислали цифру. Выводим top последних
+@bot.message_handler(func=lambda message: message.text[0] == '#')
+def handle_sharp(message):
+    bot.send_message(chat_id=message.chat.id, text='Ты прислал #')
+
 
 
 bot.polling()
--- PSQLighter.py
+++ PSQLighter.py
@@ -1,11 +1,3 @@
-# -*- coding: utf-8 -*-
-# Файл для работы с бд
-
-'''
- Для heroku придётся использовать базу данных,
- т.к. данные не сохраняются
-'''
-
 import psycopg2
 import dj_database_url
 import os
@@ -27,6 +19,7 @@ class PSQLighter:
             self.cursor = self.connection.cursor()
             self.connection.set_client_encoding('UTF8')
             self.id_feedback = None
+            self.user_id = None
             print("connection success")
         except Exception as e:
             print("Ошибка __init__: %s" % str(e))
@@ -59,7 +52,7 @@ class PSQLighter:
     def create_feedback_id(self):
         try:
             with self.connection:
-                self.cursor.execute('''insert into food_list default values RETURNING id''' )
+                self.cursor.execute('''insert into food_list(user_id) values(%s) RETURNING id''' % (self.user_id) )
                 id_of_new_row = self.cursor.fetchone()[0]
                 self.connection.commit
                 self.id_feedback = id_of_new_row
@@ -75,7 +68,7 @@ class PSQLighter:
     def set_descr(self, message):
         try:
             if self.user_id is None:
-                self.check_exist_client(self, message.user)
+                self.check_exist_client(message.from_user)
             if self.id_feedback is None:
                 self.create_feedback_id()
             with self.connection:
@@ -113,7 +106,7 @@ class PSQLighter:
                 self.cat_id = result[0]
             return self.cat_id
         except Exception as e:
-            print("Ошибка get_cat_id: %s" % str(e))
+            print("Ошибка get_category_id: %s" % str(e))
 
     # Создание ИД категории.
     def create_category_id(self, message):
@@ -131,7 +124,7 @@ class PSQLighter:
     def set_category(self, message):
         try:
             if self.user_id is None:
-                self.check_exist_client(self, message.user)
+                self.check_exist_client(message.from_user)
             if self.id_feedback is None:
                 self.create_feedback_id()
             cat_id = self.get_category_id(message)
@@ -143,104 +136,28 @@ class PSQLighter:
         except Exception as e:
             print("Ошибка set_category: %s" % str(e))
 
-
-    def set_client_phone(self, contact, username):
-        """
-        Добавляем телефон клиента
-        :param contact: Отправленный клиентом контакт
-        :param username: юзернейм клиента
-        """
-        with self.connection:
-            if self.check_exist_client(contact.user_id):
-                self.cursor.execute('''UPDATE clients SET phone_number = \'%s\' WHERE Id = %s;''' % (
-                contact.phone_number, contact.user_id))
-            else:
-                self.cursor.execute(
-                    '''INSERT INTO clients(Id, username, first_name, phone_number) VALUES(%s, \'%s\', \'%s\', \'%s\');''' % (
-                    contact.user_id, username, contact.first_name, contact.phone_number))
-        return None
-
-    def set_order(self, Id, IdClient, Item, Vol, OrderTime):
-        """
-        Добавляем заказ. Из параметров Item, Vol, OrderTime передаем только один.
-        :param Id: Id заказа.
-        :param IdClient: Id пользователя.
-        :param Item: Что покупаем.
-        :param Vol: юзернейм клиента.
-        :param OrderTime: юзернейм клиента.
-        :return: ИД добавленной записи.
-        """
+    def set_photo(self, message):
         try:
+            if self.user_id is None:
+                self.check_exist_client(message.from_user)
+            if self.id_feedback is None:
+                self.create_feedback_id()
             with self.connection:
-                cursor = self.connection.cursor()
-                # Добавляем что.
-                if (IdClient is not None):
-                    # Вставляем заказ
-                    if (Item != "") and (Id is None):
-                        cursor.execute('''INSERT INTO Orders(IdClient, Item) VALUES (%s, \'%s\') RETURNING id;''' % (
-                        IdClient, Item))
-                        id_of_new_row = cursor.fetchone()[0]
-                        self.connection.commit
-                        return id_of_new_row
-                    # Добавляем объем заказа
-                    if (Vol is not None) and (Id is not None):
-                        cursor.execute('''UPDATE Orders SET Vol = \'%s\' WHERE Id = %s ;''' % (Vol, Id))
-                        self.connection.commit
-                        return Id
-                    # Добавляем время заказа
-                    if (OrderTime is not None) and (Id is not None):
-                        dt = datetime.now()
-                        cursor.execute(
-                            '''UPDATE Orders SET OrderTime = \'%s\', DateCreate = \'%s\' WHERE Id = %s;''' % (
-                            OrderTime, dt, Id))
-                        self.connection.commit
-                        return Id
-                else:
-                    return None
+                 self.cursor.execute('''UPDATE food_list SET foto_link = \'%s\' WHERE id = %s;''' % (
+                     message.photo[-1].file_id, self.id_feedback))
+                 self.connection.commit
         except Exception as e:
-            print("Ошибка set_order: %s" % str(e))
+            print("Ошибка set_photo: %s" % str(e))
 
-    def get_order_string(self, id, tobar=0):
+    def get_lasts(self, message):
         try:
-            with self.connection:
-                cursor = self.connection.cursor()
-                # Текст заказа для клиrента
-                if tobar == 0:
-                    cursor.execute('''SELECT Item, Vol, OrderTime   FROM orders where Id = %s;''' % (id))
-                    res = cursor.fetchone()
-                    if res[1] is None:
-                        r1 = ''
-                    else:
-                        r1 = res[1]
-                    return res[0] + ', ' + r1 + ', ' + res[2]
-
-                # Текст заказа для баристы
-                elif tobar == 1:
-                    res = cursor.execute(
-                        '''SELECT Item, Vol, OrderTime, Clients.first_name, orders.Id FROM Orders LEFT JOIN Clients ON orders.IdClient = Clients.Id where Orders.Id  = %s;''' % (
-                            id))
-                    res = cursor.fetchone()
-                    if res[1] is None:
-                        r1 = ''
-                    else:
-                        r1 = res[1]
-
-                    if res is not None:
-                        return '# ' + str(res[4]) + ' Name: ' + res[3] + ', ' + res[0] + ', ' + r1 + ', ' + res[2]
-                else:
-                    return None
+            if self.user_id is None:
+                self.check_exist_client(message.from_user)
+            cursor = self.connection.cursor()
+            cursor.execute('''select categories.name, score, foto_link, descr 
+            from food_list left join categories on food_list.cat_id = categories.id 
+            where user_id = %s order by date_add desc limit %s''' % (self.user_id, message.text))
+            result = cursor.fetchone()
+            return result
         except Exception as e:
-            print("Ошибка get_order_string : %s" % str(e))
-
-    def del_order(self, id):
-        """
-        Удаляем заказ.
-        :param id: Id заказа.
-        """
-        try:
-            with self.connection:
-                if (id is not None):
-                    self.cursor.execute('''DELETE FROM Orders where Id = %s;''' % (id))
-            return None
-        except Exception as e:
-            print("Ошибка del_order : %s" % str(e))
+            print("Ошибка get_lasts: %s" % str(e))
