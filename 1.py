eec3d4d8cf4fdf76647170ee1b85970d2f15f11a
Konstantin <jashilko@gmail.com>
2021-10-29 22:24:01 +0300

    + Продолжаем локализацию #7

--- 1.py
+++ 1.py
@@ -55,31 +55,34 @@ def handle_digit(message):
         countt = 5
     answers = db_worker.get_lasts(message)
     if len(answers) == 0:
-        bot.send_message(chat_id=message.chat.id, text=_('No records in the database'))
+        text = _('No records in the database')
     elif message.text.isdigit():
         if countt > len(answers):
-            bot.send_message(chat_id=message.chat.id, text='В базе только  ' + str(len(answers)) + ' оценок: ')
+            text = _('There are only %d scores') % (len(answers))
         else:
-            bot.send_message(chat_id=message.chat.id, text='Вывожу ' + message.text + ' последних оценок: ')
+            text = _('Display %s of the latest scores: ') % (message.text)
+
     else:
-        bot.send_message(chat_id=message.chat.id, text='Вывожу '+ str(len(answers)) + ' последних оценок. Для другого количества напиши цифру: ')
+        text = _('Display %s of the latest scores. For another count, write a number: ') % len(answers)
+
+    bot.send_message(chat_id=message.chat.id, text=text)
 
     i = 1
     for ans in answers:
         if ans["score"] == 1:
-            score = " Оценка: Охуенно "
+            score = _(" Score: Amazing ")
         elif ans["score"] == -1:
-            score = " Оценка: Говно "
+            score = _(" Score: Shit ")
         else:
             score = " "
         if ans["descr"] is None:
             descr = ""
         else:
-            descr = "Описание: " + ans["descr"]
+            descr = _("Description: ") + ans["descr"]
         if ans["cat"] is None:
             cat = " "
         else:
-            cat = "Категория: #" + ans["cat"]
+            cat = _("Category: #") + ans["cat"]
         if ans["foto_link"] is not None:
             emo = '⬇'
         else:
@@ -96,24 +99,25 @@ def handle_digit(message):
 # Прислали тег. Выводим top 5 последних по тегу
 @bot.message_handler(func=lambda message: message.text[0] == '#')
 def handle_sharp(message):
-    bot.send_message(chat_id=message.chat.id, text='Вывожу 5 последних ' + message.text)
+    text = _("Display 5 of the latest scores ") + message.text
+    bot.send_message(chat_id=message.chat.id, text=text)
     answers = db_worker.get_sharp(message)
     i = 1
     for ans in answers:
         if ans["score"] == 1:
-            score = " Оценка: Охуенно "
+            score = _(" Score: Amazing ")
         elif ans["score"] == -1:
-            score = " Оценка: Говно "
+            score = _(" Score: Shit ")
         else:
             score = " "
         if ans["descr"] is None:
             descr = ""
         else:
-            descr = "Описание: " + ans["descr"]
+            descr = _("Description: ") + ans["descr"]
         if ans["cat"] is None:
             cat = " "
         else:
-            cat = "Категория: #" + ans["cat"]
+            cat = _("Category: #") + ans["cat"]
         if ans["foto_link"] is not None:
             emo = '⬇'
         else:
@@ -132,41 +136,43 @@ def callback_handler(callback_query):
     message = callback_query.message
     text = callback_query.data
     if text in ('shit', 'good'):
-        bot.send_message(chat_id=message.chat.id, text='Укажи категорию: ')
+        text = _("Set the category: ")
+        bot.send_message(chat_id=message.chat.id, text=text)
         db_worker.set_score(text)
         update_state(message, CATEGORY)
     if text == 'canceldescr':
         bot.send_message(chat_id=message.chat.id, text='Bye!')
         update_state(message, FINISH)
 
+@bot.message_handler(commands=['setting'])
+def handle_message(message):
+    keyboard = types.InlineKeyboardMarkup(row_width=2)
+    entext = 'English ' + '🇺🇸'
+    rutext = 'Русский ' + '🇷🇺'
+    b1 = types.InlineKeyboardButton(text=entext, callback_data='enlang')
+    b2 = types.InlineKeyboardButton(text=rutext, callback_data='rulang')
+    keyboard.add(b1, b2)
+    text = _("Let's set a language: ")
+    bot.send_message(chat_id=message.chat.id, text=text,
+                     reply_markup=keyboard)
+
 @bot.message_handler(commands=['start'])
 def handle_message(message):
     update_state(message, START)
-    bot.send_message(chat_id=message.chat.id, text='Пришли фото или название')
+    text = _("Upload a photo or write a name")
+    bot.send_message(chat_id=message.chat.id, text=text)
     update_state(message, PHOTO)
 
 @bot.message_handler(commands=['help'])
 def handle_message(message):
-    name = _("Mary")
-
-    # help = _('Кидай фото еды и давай оценку. \n ' \
-    #        'Бот запомнит и покажет, что хорошо, а что нет. \n ' \
-    #        'Напиши цифру, чтобы вывести столько последних оценок.\n ' \
-    #        'Напиши \"#кофе\", чтобы получить 5 последних записей про кофе')
-
-    # help_en = _('Upload a photo of the food and give an assessment. \n '
-    #             'The bot will remember and show what is good and what is not. \n '
-    #             'Write a number to output so many recent scores.\n '
-    #             'Write \"#coffee\" to get the last 5 entries about coffee')
-
-    help_en = _("This is a help")
-
+    help_en = _("Upload a photo of the food and give an assessment. \n The bot will remember and show what is good and what is not. \n Write a number to output so many recent scores.\n Write \"#coffee\" to get the last 5 entries about coffee")
     bot.send_message(chat_id=message.chat.id, text=help_en)
 
 # Начальное состояние → Просим фото.
 @bot.message_handler(func=lambda message: get_state(message) == START)
 def handle_message(message):
-    bot.send_message(chat_id=message.chat.id, text='Пришли фото или название')
+    text = _("Upload a photo or write a name")
+    bot.send_message(chat_id=message.chat.id, text=text)
     update_state(message, PHOTO)
 
 
@@ -183,18 +189,21 @@ def handle_message(message):
         db_worker.set_descr(message)
 
     keyboard = types.InlineKeyboardMarkup(row_width=2)
-    b1 = types.InlineKeyboardButton(text='💩' + ' Говно', callback_data='shit')
-    b2 = types.InlineKeyboardButton(text='Охуенно ' + '😻', callback_data='good')
+    shittext = '💩' + ''
+    goottext = 'Amazing ' + '😻'
+    b1 = types.InlineKeyboardButton(text=shittext, callback_data='shit')
+    b2 = types.InlineKeyboardButton(text=goottext, callback_data='good')
     keyboard.add(b1, b2)
-    bot.send_message(chat_id=message.chat.id, text='Оцени еду. Средней оценки нет, только плохо или хорошо',
+    text = _('Estimate the food. There is no average score, only bad or good')
+    bot.send_message(chat_id=message.chat.id, text=text,
                      reply_markup=keyboard)
     update_state(message, RATE)
-
 
 # Состояние: отправлена оценка → Просим категорию
 @bot.message_handler(func=lambda message: get_state(message) == RATE)
 def handle_message(message):
-    bot.send_message(chat_id=message.chat.id, text='Укажи категорию: ')
+    text = _("Set the category: ")
+    bot.send_message(chat_id=message.chat.id, text=text)
     update_state(message, CATEGORY)
 
 
@@ -204,10 +213,11 @@ def handle_message(message):
     # Обработка категории.
     db_worker.set_category(message)
     keyboard = types.InlineKeyboardMarkup(row_width=1)
-    b1 = types.InlineKeyboardButton(text='Не хочу', callback_data='canceldescr')
+    notext = _("Not want")
+    b1 = types.InlineKeyboardButton(text=notext, callback_data='canceldescr')
     keyboard.add(b1)
-    bot.send_message(chat_id=message.chat.id, text='Категория #' + message.text +
-                                                   ' установлена. Хочешь что-то добавить к описанию?', reply_markup=keyboard)
+    text = _('Category #%s is set. Do you want to add something to the description?') % message.text
+    bot.send_message(chat_id=message.chat.id, text=text, reply_markup=keyboard)
     update_state(message, DESCR)
 
 
@@ -222,8 +232,6 @@ def handle_message(message):
 @bot.message_handler(commands=['setting'])
 def handle_message(message):
     bot.send_message(chat_id=message.chat.id, text='Coming soon')
-
-
 
 
 bot.polling()
