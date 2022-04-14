172f0819e92db2d8eb9d56f444634ef0ac4bc0aa
Konstantin <jashilko@gmail.com>
2022-04-15 00:04:29 +0300

    Update run.py

--- SQLAlchemy/run.py
+++ SQLAlchemy/run.py
@@ -2,14 +2,10 @@ import sqlalchemy
 from models import metadata, categories
 from sqlalchemy import create_engine
 
-engine = create_engine("postgresql+psycopg2://knqprsizhfmhvo:06eac893975210ab6778d1efaa96f0e5536c71b20a244a6496309d53401514b9@ec2-44-199-83-229.compute-1.amazonaws.com/d55qo5k71vl8lg")
 
 engine.connect()
 #print(engine)
 
-# #database = PostgresqlDatabase('d55qo5k71vl8lg', user='knqprsizhfmhvo',
-#                               password='06eac893975210ab6778d1efaa96f0e5536c71b20a244a6496309d53401514b9',
-#                               host='ec2-44-199-83-229.compute-1.amazonaws.com')
 
 # for t in metadata.tables:
 #     print(metadata.tables[t])
