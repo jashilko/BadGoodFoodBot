18e91422f9a702a1468126bfc9adfb4766f1b17c
Konstantin <jashilko@gmail.com>
2022-04-17 23:45:59 +0300

    + Поиск N последних в контроллере + Поиск по категории в контроллере + Удаление записи в контр + Атрибут conn в конт

--- SQLAlchemy/controller.py
+++ SQLAlchemy/controller.py
@@ -1,24 +1,75 @@
-from sqlalchemy import select
-from models import food_list
+from sqlalchemy import select, and_, desc, func, delete
+from models import food_list, categories
 
 class DBWorker:
-    def __init__(self, user_id):
+    def __init__(self, conn, user_id):
         self.user_id = user_id
+        self.conn = conn
     def get_lasts(self, count):
         '''
         Получить последние записи пользователя
         '''
-        s = select([food_list])
+        s = select([
+            categories.c.name,
+            food_list.c.score,
+            food_list.c.foto_link,
+            food_list.c.descr,
+            food_list.c.id
+        ]).select_from(
+            food_list.join(categories)
+        ).where(
+            food_list.c.user_id == self.user_id
+        ).limit(count)
         return s
 
     def get_sharp(self, sharp):
         '''
         Получить 5 последних по тегу
         '''
-        pass
+        s = select([
+            categories.c.name,
+            food_list.c.score,
+            food_list.c.foto_link,
+            food_list.c.descr,
+            food_list.c.id
+        ]).select_from(
+            food_list.join(categories)
+        ).where(
+            and_(
+                food_list.c.user_id == self.user_id,
+                func.upper(categories.c.name) == func.upper(sharp),
+                food_list.c.score != None,
+                categories.c.name != None
+            )
+        ).limit(5).order_by(
+                desc(food_list.c.date_add)
+            )
+        return s
 
     def del_feedback(self, id_feedback):
         '''
         Удалить запись
         '''
-
+        # Проверяем, есть ли такая запись
+        s = select([
+            food_list]
+        ).where(
+            and_(
+                food_list.c.user_id == self.user_id,
+                food_list.c.id == id_feedback
+            )
+        )
+        if self.conn.execute(s).rowcount == 0:
+            return "Not_Found"
+        else:
+            # Удаляем существующую запись
+            d = delete(
+                food_list
+            ).where(
+                and_(
+                    food_list.c.user_id == self.user_id,
+                    food_list.c.id == id_feedback
+                )
+            )
+            rs = self.conn.execute(d)
+            return "Ok"
--- SQLAlchemy/run.py
+++ SQLAlchemy/run.py
@@ -1,6 +1,7 @@
 import sqlalchemy
 from models import metadata, categories
 from sqlalchemy import create_engine
+from controller import DBWorker
 import controller
 import os
 
@@ -13,10 +14,17 @@ engine.connect()
 # for t in metadata.tables:
 #     print(metadata.tables[t])
 
-s = categories.select()
-print(s)
+#s = categories.select()
+#print(s)
 
 conn = engine.connect()
-s = categories.select()
-r = conn.execute(s)
-print(r.fetchall())
+w = DBWorker(conn, '2150772')
+#s = w.get_lasts(4)
+#s = categories.select()
+#r = conn.execute(s)
+#print(r.fetchall())
+
+#s = w.get_sharp('пиво')
+#r = conn.execute(s)
+#rint(r.fetchall())
+print(w.del_feedback(33))
