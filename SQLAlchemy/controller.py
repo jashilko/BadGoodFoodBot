53f4331e7d4b011891632a3ec6249e9a86f7a31d
jashilko-hp <jashilko@gmail.com>
2022-04-28 19:44:52 +0300

    Добавляем поиск по тегу своих и друзей

--- SQLAlchemy/controller.py
+++ SQLAlchemy/controller.py
@@ -85,17 +85,70 @@ class DBWorker:
             row = fe.fetchall()
             flist = list(row[0])
         flist.append(int(self.user_id))
-        # u = union(
-        #     [select([
-        #     categories.c.name,
-        #     food_list.c.score,
-        #     food_list.c.foto_link,
-        #     food_list.c.descr,
-        #     food_list.c.id
-        # ]).select_from(
-        #     food_list.join(categories)
-        # ).where(
-        #     food_list.c.user_id == c
-        # ) for c in flist]
-        # )
-        return flist
+        slist = []
+        for one in flist:
+            s = select([
+                categories.c.name,
+                food_list.c.score,
+                food_list.c.foto_link,
+                food_list.c.descr,
+                food_list.c.id
+            ]).select_from(
+                food_list.join(categories)
+            ).where(
+                food_list.c.user_id == one
+            )
+            slist.append(s)
+        u = union(*slist)
+        answers = []
+        ru = self.conn.execute(u)
+        results = ru.fetchone()
+        while results is not None:
+            answers.append({"cat": results[0], "score": results[1], "foto_link":
+                results[2], "descr": results[3], "id": results[4]})
+            results = ru.fetchone()
+        return answers
+
+    def get_sharp_friends(self, sharp):
+        '''
+        Получаем список по тегу своих и друзей
+        '''
+        flist = []
+        f = select([user_friends.c.friend_id]).where(user_friends.c.user_id == self.user_id)
+        fe = self.conn.execute(f)
+        if fe.rowcount > 0:
+            row = fe.fetchall()
+            flist = list(row[0])
+        flist.append(int(self.user_id))
+        slist = []
+        for one in flist:
+            s = select([
+                categories.c.name,
+                food_list.c.score,
+                food_list.c.foto_link,
+                food_list.c.descr,
+                food_list.c.id,
+                food_list.c.user_id,
+                food_list.c.date_add
+            ]).select_from(
+                food_list.join(categories)
+            ).where(
+                and_(
+                    food_list.c.user_id == one,
+                    func.upper(categories.c.name) == func.upper(sharp),
+                    food_list.c.score != None,
+                    categories.c.name != None
+                )
+            )
+            slist.append(s)
+        u = union(*slist).order_by(
+                desc(food_list.c.date_add)
+            )
+        answers = []
+        ru = self.conn.execute(u)
+        results = ru.fetchone()
+        while results is not None:
+            answers.append({"cat": results[0], "score": results[1], "foto_link":
+                results[2], "descr": results[3], "id": results[4], "user_id": results[5]})
+            results = ru.fetchone()
+        return answers
